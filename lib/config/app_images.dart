b06080f788b73e6308d568a0f151abfcb235c455
kasunhasanga <kasunhasanga96@gmail.com>
2025-08-13 21:29:35 +0530

    Change app Name and logo to Kast

--- lib/config/app_images.dart
+++ lib/config/app_images.dart
@@ -3,7 +3,7 @@ class AllImages{
   static final AllImages _instance = AllImages._();
   factory AllImages() => _instance;
 
-  String splashLogo = 'assets/app_icon/production.jpeg';
+  String splashLogo = 'assets/app_icon/play_store_512.png';
   String startupBackground = 'assets/images/startup/startup_background_image.png';
 
 
